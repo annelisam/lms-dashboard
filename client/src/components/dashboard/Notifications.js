fa376976f7af03d649c398e6cb7501aa79bc149d
annelisam <annelisamm@icloud.com>
2019-12-19 14:45:53 -0800

    updates to project repo

--- client/src/components/dashboard/Notifications.js
+++ client/src/components/dashboard/Notifications.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Notifications = () => {
-    return (
-        <div>
-            <p>
-                Notifications
-            </p>
+  return (
+    <div className="section notifications">
+      <h5>Notifications</h5>
+      <div className="card z-depth-1">
+        <div className="card-content">
+          <span className="card-title">hello</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Notifications;
--- client/src/components/projects/ProjectList.js
+++ client/src/components/projects/ProjectList.js
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
+      <h5>Projects</h5>
       {projects &&
-        projects.map((project) => {
+        projects.map(project => {
           return (
-            <Link to={'/project/' + project.id} key={project.id}>
-              <ProjectSummary project={project} />
+            <Link to={"/project/" + project.id}>
+              <ProjectSummary project={project} key={project.id} />
             </Link>
-          )
+          );
         })}
     </div>
   );
